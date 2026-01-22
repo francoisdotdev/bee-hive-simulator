3fb0948960507e001bf43790eaff5cbbfc31c315
francoisdotdev <lef02fran@gmail.com>
2026-01-22 22:23:58 +0100

    refactor: restructure Bee model by converting classes to types and enhancing type definitions

--- src/models/Bee.ts
+++ src/models/Bee.ts
@@ -1,58 +1,92 @@
-export enum Sex {
-    male= 'Male',
-    female= 'Female'
+import type { Season, FoodSource } from "./World";
+import type { ValueOf } from "../types";
+
+// type Bee = Queen | Worker | Drone;
+// type Sex = 'male' | 'female';
+// type WorkerRole = 'cleaner' | 'larvaeNurse' | 'queenNurse' | 'builder'| 'handler' | 'fan' | 'guardian' | 'forager';
+
+const Bee = {
+    Queen: 'queen',
+    Worker: 'worker',
+    Drone: 'drone'
+} as const;
+
+const Sex = {
+    Male: 'male',
+    Female: 'female'
+} as const;
+
+//Rôle basé sur l'age et les besoins de la ruche
+const WorkerRole = {
+    Cleaner: 'cleaner',
+    LarvaeNurse: 'larvaeNurse',
+    QueenNurse: 'queenNurse',
+    Builder: 'builder',
+    Handle: 'handler',
+    Fan: 'fan',
+    Guardian: 'guardian',
+    Forage: 'forager'
+} as const;
+
+const LarvaStage = {
+    Egg: 'egg',
+    Larva: 'larva',
+    Nymph: 'nymph'
+} as const;
+
+type BaseBee = {
+    age: number,
+    health: number,
+    stress: number
 }
 
-export enum Season {
-    spring= 'Spring',
-    summer= 'Summer',
-    autumn= 'Autumn',
-    winter= 'Winter'
+type Queen = BaseBee & {
+    sex: typeof Sex.Female,
+    maxLifeSpan: 1820, // (5 ans, 260 weeks)
+    fertility: number,
+    isVirgin: boolean,
+    pheromoneStrength: number,
+    spermCount: number,
+    laidEggsToday: number
+
 }
 
-export abstract class Bee {
-
-    constructor(
-        public age: number,
-        public sex: Sex,
-        public health: number,
-        public maturity: number,
-        public readonly maxLifespan: number
-        ) {}
+type Worker = BaseBee & {
+    sex: typeof Sex.Female,
+    role: ValueOf<typeof WorkerRole>,
+    maxLifeSpan: number,
+    juvenileHormone: number, //si élevé -> vieillit + vite, devient butineuse, sort de la ruche, vieillit - longtemps
+    vitellogenin: number, //si élevé -> jeune physiologiquement, tache interieur, vit + longtemps, resiste mieux au stress
+    fatBodies: number,
+    foragingExperience: number,
+    knowFoodSources: FoodSource[]
+}
+/* 
+Jeune abeille    →    VG haute, JH basse    →    Nourrice
+↓ avec l'âge
+Abeille mature   →    VG basse, JH haute    →    Butineuse
+*/
+type Drone = BaseBee & {
+    sex: typeof Sex.Male,
+    maxLifeSpan: 90,
+    hasFlownToday: boolean,
+    maturity: number
 }
 
-export class Queen extends Bee {
-    constructor(
-        age: number,
-        health: number,
-        maturity: number,
-        public fertility: number,
-        public pheromoneStrength: number,
-        public isVirgin: boolean,
-        public spermCount: number,
-        public eggsLaidToday: number
-    ) {
-        super(age, Sex.female, health, maturity, 260)
-    }
+type Larvae = BaseBee & {
+    stage: ValueOf<typeof LarvaStage>,
+    daysInStage: number,
+    isFertilized: boolean,
+    caste: ValueOf<typeof Bee>,
+    fedRoyalJelly: boolean,
+    temperature: number
 }
 
-export class Worker extends Bee {
-    constructor(
-        age: number,
-        health: number,
-        maturity: number,
-        season: Season
-    ) {
-        super(age, Sex.female, health, maturity, ( season === Season.spring || season === Season.summer ? 6 : 24))
-    }
-}
-
-export class Drone extends Bee {
-    constructor(
-        age: number,
-        health: number,
-        maturity: number
-    ) {
-        super(age, Sex.male, health, maturity, 8)
-    }
+type WaggleDance = {
+    direction: number, //angle
+    distance: number, //metre
+    duration: number, //seconde
+    quality: number, // 0-1
+    foodSource: FoodSource,
+    followers: Worker[]
 }
